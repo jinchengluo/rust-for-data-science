fcae9db5aeb61c81d57234cd1f74976c4aad1fae
Vincent Lo <vincent.lo@telecom-sudparis.eu>
2025-07-22 15:38:54 +0200

    removed code into TODOs and generated profiler

--- poly_match_rs/src/lib_v2.rs
+++ poly_match_rs/src/lib_v2.rs
@@ -6,24 +6,14 @@ use numpy::{PyArray1, PyReadonlyArray1, ToPyArray};
 
 #[pyclass(subclass)]
 struct Polygon {
-    x: Array1<f64>,
-    y: Array1<f64>,
-    center: Array1<f64>,
+    // TODO
 }
 
 #[pymethods]
 impl Polygon {
     #[new]
     fn new(x: PyReadonlyArray1<f64>, y: PyReadonlyArray1<f64>) -> Polygon {
-        let x = x.as_array();
-        let y = y.as_array();
-        let center = Array1::from_vec(vec![x.mean().unwrap(), y.mean().unwrap()]);
-
-        Polygon {
-            x: x.to_owned(),
-            y: y.to_owned(),
-            center,
-        }
+        // TODO
     }
 
     #[getter]
@@ -48,21 +38,10 @@ fn find_close_polygons<'py>(
     point: PyReadonlyArray1<'py, f64>,
     max_dist: f64,
 ) -> PyResult<Vec<Bound<'py, Polygon>>> {
-    let mut close_polygons = vec![];
-    let point = point.as_array();
-    for poly in polygons {
-        let center = poly.borrow().center.to_owned();
-
-        if (center - point).norm() < max_dist {
-            close_polygons.push(poly)
-        }
-    }
-
-    Ok(close_polygons)
+    // TODO
 }
 
 pub fn poly_match_rs(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
-    m.add_class::<Polygon>()?;
-    m.add_function(wrap_pyfunction!(find_close_polygons, m)?)?;
+    // TODO
     Ok(())
 }
--- poly_match_rs/src/lib_v3.rs
+++ poly_match_rs/src/lib_v3.rs
@@ -6,24 +6,14 @@ use numpy::{PyArray1, PyReadonlyArray1, ToPyArray};
 
 #[pyclass(subclass)]
 struct Polygon {
-    x: Array1<f64>,
-    y: Array1<f64>,
-    center: Array1<f64>,
+    // TODO
 }
 
 #[pymethods]
 impl Polygon {
     #[new]
     fn new(x: PyReadonlyArray1<f64>, y: PyReadonlyArray1<f64>) -> Polygon {
-        let x = x.as_array();
-        let y = y.as_array();
-        let center = Array1::from_vec(vec![x.mean().unwrap(), y.mean().unwrap()]);
-
-        Polygon {
-            x: x.to_owned(),
-            y: y.to_owned(),
-            center,
-        }
+        // TODO
     }
 
     #[getter]
@@ -48,25 +38,10 @@ fn find_close_polygons<'py>(
     point: PyReadonlyArray1<'py, f64>,
     max_dist: f64,
 ) -> PyResult<Vec<Bound<'py, Polygon>>> {
-    let mut close_polygons = vec![];
-    let point = point.as_array();
-    for poly in polygons {
-        let norm = {
-            let center = &poly.borrow().center;
-
-            ((center[0] - point[0]).square() + (center[1] - point[1]).square()).sqrt()
-        };
-
-        if norm < max_dist {
-            close_polygons.push(poly)
-        }
-    }
-
-    Ok(close_polygons)
+    // TODO
 }
 
 pub fn poly_match_rs(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
-    m.add_class::<Polygon>()?;
-    m.add_function(wrap_pyfunction!(find_close_polygons, m)?)?;
+    // TODO
     Ok(())
 }
